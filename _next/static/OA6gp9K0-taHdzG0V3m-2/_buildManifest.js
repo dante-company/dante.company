6303cc8974ee2816409c65fe5b6a33a4829b2658
1000ship <1000ship@users.noreply.github.com>
2022-01-27 11:43:16 +0000

    deploy: 64f6224678be1a0e4cb88d0aa6d69865298c8c37

---
+++ _next/static/OA6gp9K0-taHdzG0V3m-2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ab0a212a54967ddd.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
