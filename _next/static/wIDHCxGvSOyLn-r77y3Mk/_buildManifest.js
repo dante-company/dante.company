04ea417bcbcaac94dbf350f1ce1046350e99f609
1000ship <1000ship@users.noreply.github.com>
2022-02-20 15:56:27 +0000

    deploy: a0d2e01798fe0c69686a478da1e7eac2cd50767e

---
+++ _next/static/wIDHCxGvSOyLn-r77y3Mk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9e4a9d0a97617e0a.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
