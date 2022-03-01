21d91914ea0b896c98e4debac8a01b8a7db0a5c7
Dante Chun <dev.1000ship@gmail.com>
2022-03-01 12:47:41 +0900

    NextSEO 모듈 추가 및 사용

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,10 +1,10 @@
+import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { createGlobalStyle } from 'styled-components';
 import { Reset } from 'styled-reset';
 import 'tailwindcss/tailwind.css';
 import { BackgroundParticles, Footer } from '../components';
-import { Metadata } from '../constants/seo';
 import '../styles/globals.css';
 
 export const GlobalStyle = createGlobalStyle`
@@ -18,8 +18,27 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <>
       <Reset />
       <GlobalStyle />
+      <DefaultSeo
+        title="단테컴퍼니 | Dante Company"
+        description="단테컴퍼니는 1인 개발사로 고객사와 직접적으로 소통하고 합리적인 비용으로 개발합니다."
+        canonical="https://dante.company"
+        openGraph={{
+          url: 'https://dante.company',
+          title: '단테컴퍼니 | Dante Company',
+          description:
+            '단테컴퍼니는 1인 개발사로 고객사와 직접적으로 소통하고 합리적인 비용으로 개발합니다.',
+          images: [
+            {
+              url: 'https://dante.company/dante-company.png',
+              alt: '단테컴퍼니 로고',
+              width: 1200,
+              height: 1200,
+            },
+          ],
+          site_name: '단테컴퍼니',
+        }}
+      />
       <Head>
-        <Metadata />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="theme-color" content="#FFFFFF" />
 
