c0713ec41ca36db4cd8383baec6e85dc6ab1e4aa
Dante Chun <dev.1000ship@gmail.com>
2023-06-24 17:53:14 +0900

    Modify SEO configurations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,13 +11,32 @@ const font = Noto_Sans({
 export const metadata: Metadata = {
   title: {
     template: "%s | Dante Company",
-    default: "단테컴퍼니",
+    default: "Dante Company",
   },
   description:
     "단테컴퍼니는 1인 개발사로 국내 및 해외 기업들에게 소프트웨어 개발을 제공하며 동시에 자유의지로 만들고 싶은 것들을 개발하고 있습니다.",
   viewport: "width=device-width, initial-scale=1.0",
-  themeColor: "#FFFFFF",
+  themeColor: [
+    {
+      color: "#ffffff",
+      media: "(prefers-color-scheme: light)",
+    },
+    {
+      color: "#000000",
+      media: "(prefers-color-scheme: dark)",
+    },
+  ],
+  alternates: {
+    canonical: "https://dante.company",
+  },
   openGraph: {
+    title: "단테컴퍼니",
+    description:
+      "단테컴퍼니는 1인 개발사로 국내 및 해외 기업들에게 소프트웨어 개발을 제공하며 동시에 자유의지로 만들고 싶은 것들을 개발하고 있습니다.",
+    url: "https://dante.company",
+    siteName: "단테컴퍼니",
+    type: "website",
+    locale: "ko_KR",
     images: [
       {
         url: "https://dante.company/og/dante-company.png",
