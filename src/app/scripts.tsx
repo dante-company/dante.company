216f75de3d60aeca28b6af102e1b5e6dd2bfb365
Dante Chun <dev.1000ship@gmail.com>
2024-01-23 20:49:19 +0800

    Add adsense script to Scripts component

--- src/app/scripts.tsx
+++ src/app/scripts.tsx
@@ -35,6 +35,12 @@ const Scripts = () => {
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700025991079512"
         crossOrigin="anonymous"
       />
+      <Script
+        id="adsense"
+        dangerouslySetInnerHTML={{
+          __html: `(adsbygoogle = window.adsbygoogle || []).push({});`,
+        }}
+      />
 
       {/* Naver Search Advisor */}
       <meta
